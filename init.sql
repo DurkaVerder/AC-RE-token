b8ef1a0f5cf3fc13c249a5970fbb81f5cbf9877c
DurlaVerder <vrrrr227@gmail.com>
2025-04-19 12:51:21 +0300

    fix init sb

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 -- Table for refresh token
 
 CREATE TABLE users (
-    id GUID PRIMARY KEY,
+    id UUID PRIMARY KEY UNIQUE,
     name VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -9,12 +9,12 @@ CREATE TABLE users (
 );
 
 INSERT INTO users (id, name, email, password) VALUES
-("1e2d3c4b-5a6f-7g8h-9i0j-k1l2m3n4o5p6", "Tim", "vrrrr228@gmail.com", "1234");
+('1e2d3c4b-5a6f-4f8a-9c0d-a1b2c3d4e5f6', 'Tim', 'vrrrr228@gmail.com', '1234');
 
 
 CREATE TABLE refresh_token (
     id SERIAL PRIMARY KEY,
-    user_id GUID NOT NULL,
+    user_id UUID NOT NULL,
     token VARCHAR(255) NOT NULL,
     access_tokenId VARCHAR(255) NOT NULL,
     ip_address VARCHAR(255) NOT NULL,
